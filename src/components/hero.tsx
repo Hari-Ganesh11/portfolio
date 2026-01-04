caceef30ea92a6760937845e13c680c5b824be05
Hari-Ganesh11 <hariganeshrd@gmail.com>
2026-01-05 01:02:41 +0530

    image fix

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,7 +3,7 @@
 const Hero = () => {
   return (
     <section id='hero' className='hero-section inline-flex flex-col items-center w-full p-10'>
-      <img src='src\assets\hari_image-new.jpeg'width={200} height={200} className=' rounded-full border-2'></img>
+      <img  src={`${import.meta.env.BASE_URL}hari_image-new.jpeg`}width={200} height={200} className=' rounded-full border-2'></img>
       <div className='title-section flex flex-row'><p className=''>Hi I'm </p><h1 className='px-2'> Hari Ganesh </h1></div>
       <p className='items-center justify-center text-center'> Front end Engineer building scalable and high performance web applications for enterprises using React and Next js</p>
     </section>
